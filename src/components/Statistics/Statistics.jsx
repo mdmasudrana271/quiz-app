d5ef0df85521ad9e439d4f8f0642781d00c1f169
Masud <mr7123619@gmail.com>
2022-10-11 03:11:29 +0600

    added chart on statistics route

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useContext } from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+import { TopicsContext } from "../../Layout/Main";
 
 const Statistics = () => {
-    return (
-        <div>
-            <h3>this is statistics</h3>
+  const topics = useContext(TopicsContext);
+  const data = topics.map((topic) => topic);
+  //   console.log(total);
+  return (
+    <div className="  my-10">
+      <div>
+        <h1 className="text-3xl font-bold text-indigo-500">Data Analysis</h1>
         </div>
-    );
+      <div className="  my-10">
+        <LineChart
+          width={600}
+          height={300}
+          data={data}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <Line type="monotone" dataKey="total" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
